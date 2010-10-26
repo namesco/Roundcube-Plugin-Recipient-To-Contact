2a33ea2280a05d76fc54bb5f316f20f2b4bb3d44
vminakov <vminakov@names.co.uk>
2010-10-26 16:52:52 +0100

    Initial commit

---
+++ recipient_to_contact.js
@@ -0,0 +1,237 @@
+/**
+ * Clients scripts for recipient_to_contact plugin.
+ *
+ * Draws the jQuery UI Dialog with a form a to create new contacts. Pre-fills email addresses
+ * and contact names. Allows multiple creation of new contacts.
+ *
+ * @category  RoundCube
+ * @package   Plugin
+ * @author    Vladimir Minakov <vminakov@names.co.uk>
+ * @copyright 2009-2010 Namesco Limited
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
+ * @version   0.1
+ */
+
+/**
+ * Initializer. Draws the dialog and requests contact data from server.
+ *
+ * @return void
+ */
+rcmail.addEventListener('init', function() {
+    rcmail.addEventListener('plugin.recipient_to_contact_populate_dialog', recipient_to_contact.populate_dialog);
+
+    // create dialog container
+    var dialog = $('<div></div>').attr('id', 'new-contacts-dialog')
+                 .appendTo(document.body);
+
+    // due to weirdness of session handling in roundcube, the request to fetch the contacts should be postponed
+    setTimeout("rcmail.http_post('plugin.recipient_to_contact_get_contacts', {})", 500);
+});
+
+/**
+ * Namespace for the recipient_to_contact plugin.
+ */
+var recipient_to_contact = {
+
+    /**
+    * Generates dialog contect (form for adding new contacts), adds event listeners for ajax requests.
+    *
+    * Called, when the event plugin.recipient_to_contact_populate_dialog is triggered. Expects response data
+    * from server.
+    *
+    * @parameter array $response Response data. Contains new contacts and address books.
+    *
+    * @return void
+    */
+    populate_dialog: function(response)
+    {
+        // break if the reponse contains no data
+        if (response.length == 0) {
+            return;
+        }
+
+        // create the table and table header
+        var table = $('<table></table>').append(
+            $('<tr>').append("<td>" + rcmail.gettext('dialog_contact_name', 'recipient_to_contact') + "</td>"
+                           + "<td>" + rcmail.gettext('dialog_email', 'recipient_to_contact') + "</td>"
+                           + "<td><input type='checkbox' id='new-contacts-select-all' /></td>")
+        ).appendTo($('#new-contacts-dialog'));
+    
+
+        // iterate over new contacts contacts and generate a table row for each contact
+        $.each(response.contacts, function(key, contact) {
+            var row = $('<tr></tr>');
+
+            // generate table cell for contact's name.
+            // if contact's name has been recognized by server-side script, prefill the textfiled with it.
+            // otherwise, use email's part before '@' as contact's name
+            $('<td></td>').append(
+                $('<input />').attr('type', 'text').attr('name', '_contacts[' + key + '][_name]').val(
+                    (contact['mailto'] == contact['name'])
+                        ? contact['mailto'].split('@')[0]
+                        : contact['name']
+                )
+            ).appendTo(row);
+
+            // generate table cell for contact's email address
+            $('<td></td>').append(
+                $('<input />').attr('type', 'text').attr('name', '_contacts[' + key + '][_email]').val(contact['mailto'])
+            ).appendTo(row);
+
+            // generate dropdwon list with address books
+            $('<td></td>').append(
+                $('<input />').attr('type', 'checkbox')
+            ).appendTo(row);
+
+            row.appendTo(table);
+        });
+
+        // add 'Select All' functionality to the checbox in the form header
+        $('#new-contacts-select-all').click(function() {
+           var checked_status = this.checked;
+           $(this).parents('table').find('input:checkbox').each(function() {
+               this.checked = checked_status;
+           });
+        });
+
+        // generate select box for addressbooks
+        $('<div></div>').css('text-align', 'center').css('margin-top', '15px')
+        .text(rcmail.gettext('dialog_add_selected', 'recipient_to_contact')).append(
+            function() {
+                var select = $('<select></select>').attr('name', '_addressbook')
+                select.change(recipient_to_contact.add_new_contact);
+                select.append(
+                    $('<option></option>').val('none').text('')
+                );
+                $.each(response.address_books, function(key, address_book) {
+                    select.append(
+                        $('<option></option>').val(key).text(address_book['name'])
+                    );
+                });
+                return select;
+            }()
+        ).appendTo($('#new-contacts-dialog'));
+
+        // initialize the dialog
+        recipient_to_contact.show_dialog();
+    },
+
+    /**
+     * Adds new contact to the selected addressbook.
+     *
+     * Performs client-side validation and sends request to server. Triggered when by onChange event
+     * on the addressbooks' select box.
+     *
+     * @return void
+     */
+    add_new_contact: function()
+    {
+        // verify that at least one checkbox has been selected. ignore the very first checkbox (Select All)
+        var checked = $('#new-contacts-dialog').find('input:checked').not('#new-contacts-select-all');
+        if (checked.length == 0) {
+            rcmail.display_message(rcmail.gettext('response_none_selected', 'recipient_to_contact'), 'error');
+            $('#new-contacts-dialog select option:eq(0)').attr('selected', 'selected');
+            return;
+        }
+
+        // stores new contacts that will be sent to server
+        var new_contacts = [];
+
+        var is_valid = true;
+        
+        checked.each(function() {
+           // for each selected checkbox find corresponding input fields with contact's name and email
+           var contact = $(this).parents('tr').first().find('input:text');
+
+           // validate contact's name
+           if ($(contact[0]).val() == '') {
+               rcmail.display_message(rcmail.gettext('response_name_empty', 'recipient_to_contact'), 'error');
+               $('#new-contacts-dialog select option:eq(0)').attr('selected', 'selected');
+               is_valid = false;
+               return;
+           }
+
+           // validate email
+           if (!rcube_check_email($(contact[1]).val())) {
+               rcmail.display_message(rcmail.gettext('response_email_invalid', 'recipient_to_contact'), 'error');
+               $('#new-contacts-dialog select option:eq(0)').attr('selected', 'selected');
+               is_valid = false;
+               return;
+           }
+
+           new_contacts.push(contact[0]);
+           new_contacts.push(contact[1]);
+        });
+
+        // if there weren't any validation errors, serialize new contacts data and send it to server
+        if (is_valid) {
+            var data = $(new_contacts).add($('#new-contacts-dialog select')).serialize();
+
+            rcmail.addEventListener('plugin.recipient_to_contact_add_contact_response', recipient_to_contact.add_contact_handler);
+            rcmail.http_post('plugin.recipient_to_contact_add_contact', data);
+        }
+
+    },
+
+    /**
+     * Initializes the Jquey UI Dialog for adding new contacts.
+     *
+     * @return void
+     */
+    show_dialog: function()
+    {
+        $('#new-contacts-dialog').dialog({
+            autoResize: true,
+            modal: true,
+            resizable: false,
+            width: 'auto',
+            title: rcmail.gettext('dialog_title', 'recipient_to_contact'),
+            buttons: {
+    				Close: function() {
+    					$(this).dialog( "close" );
+    				}
+        	}
+        });
+    },
+
+    /**
+     * Handles the add contact response from server.
+     *
+     * Removes the row from dialog, if the contact has been added successfully
+     * or shows validation errors.
+     * 
+     * @param array $response Response array from server.
+     *
+     * @return void
+     */
+    add_contact_handler: function(response)
+    {
+        var error_messages = [];
+
+        // iterate over contacts recieved from server.
+        // Remove rows from table, which have been added successfully or populate error_messages array.
+        $.each(response.contacts, function(key, contact) {
+            if (contact.status == 'ok') {
+                $("#new-contacts-dialog input[name^=_contacts\\[" + key + "\\]]").parents('tr').remove();
+            } else {
+                error_messages.push(contact.message);
+            }
+        });
+
+        // if all contacts have been added successfully, show confirmation message and delete the dialog.
+        if (error_messages.length == 0) {
+            rcmail.display_message(rcmail.gettext('response_confirm', 'recipient_to_contact'), 'confirmation');
+
+            if ($('#new-contacts-dialog table tr').length == 1) {
+                $('#new-contacts-dialog').remove();
+            }
+        } else {
+            // otherwise show all errors
+            $.each(error_messages, function() {
+                rcmail.display_message(this, 'error');
+            });
+        }
+
+        $('#new-contacts-dialog select option:eq(0)').attr('selected', 'selected');
+    }
+}
---
+++ recipient_to_contact.php
@@ -0,0 +1,348 @@
+<?php
+/**
+ * Plugin allows users to add new contacts to address books.
+ *
+ * @category  Roundcube
+ * @package   Plugin
+ * @author    Vladimir Minakov <vminakov@names.co.uk>
+ * @copyright 2009-2010 Namesco Limited
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
+ * @version   0.1
+ */
+
+/**
+ * Short description of new_contacts
+ *
+ * If a user sends an email to recipients, which are not in addressbooks, the plugins allows users
+ * to selectively add those recipients to existing addressbook.
+ *
+ * @category  Roundcube
+ * @package   Plugin
+ * @author    Vladimir Minakov <vminakov@names.co.uk>
+ * @copyright 2009-2010 Namesco Limited
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
+ * @version   0.1
+ */
+class recipient_to_contact extends rcube_plugin
+{
+    /**
+     * Plugin is used for 'mail' and 'settings' tasks.
+     *
+     * @var string
+     */
+    public $task = 'mail|settings';
+
+    /**
+     * Instance of rcmail.
+     *
+     * @var rcmail
+     */
+    protected $rcmail;
+
+    /**
+     * List of addressbooks for searching existing and adding new contacts.
+     *
+     * @var array
+     */
+    protected $addressbooks = array();
+    
+    /**
+     * Plugins initializer.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->rcmail = rcmail::get_instance();
+
+        // load configuration
+        if (file_exists($this->home . '/config/config.inc.php')) {
+            $this->load_config('config/config.inc.php');
+        } else {
+            $this->load_config('config/config.inc.php.dist');
+        }
+
+        $is_enabled = $this->rcmail->config->get('use_recipienttocontact', 'default');
+
+        if ($is_enabled === 'default') {
+            $is_enabled = $this->rcmail->config->get('recipient_to_contact_enabled_by_default');
+            $this->rcmail->config->set('use_recipienttocontact', $is_enabled);
+        }
+
+        // register hooks and actions if the plugin is enabled (by default or though preferences section)
+        if ($is_enabled) {
+            $this->add_hook('message_sent', array($this, 'check_recipients'));
+            $this->add_hook('render_mailboxlist', array($this, 'register_dialog'));
+
+            $this->register_action('plugin.recipient_to_contact_get_contacts', array($this, 'get_contacts'));
+            $this->register_action('plugin.recipient_to_contact_add_contact', array($this, 'add_contact'));
+
+            // fetch addressbook sources. If no addressbooks set in the config file, use the same addressbooks
+            // configured for autocompletition
+            $enabled_addressbooks = $this->rcmail->config->get('recipient_to_contact_addressbooks');
+            if (empty($enabled_addressbooks)) {
+                $this->addressbooks = $this->get_addressbooks(
+                        $this->rcmail->config->get('autocomplete_addressbooks'));
+            } else {
+                $this->addressbooks = $this->get_addressbooks($enabled_addressbooks);
+            }
+
+        }
+        // hooks for preferences section
+        $this->add_hook('preferences_list', array($this, 'prefs_content'));
+        $this->add_hook('preferences_sections_list', array($this, 'prefs_section_link'));
+        $this->add_hook('preferences_save', array($this, 'prefs_save'));
+
+        $this->add_texts('localization', true);
+    }
+
+    /**
+     * Checks wether recipients exist in any of the addressbooks.
+     *
+     * @param array $args Default hook parameters.
+     *
+     * @return void
+     */
+    public function check_recipients($args)
+    {
+        $rcube_imap = new rcube_imap(null);
+
+        // build recipients array
+        $recipients = $rcube_imap->decode_address_list($args['headers']['To']);
+        if (isset($args['headers']['Cc'])) {
+            $recipients = array_merge($recipients, $rcube_imap->decode_address_list($args['headers']['Cc']));
+        }
+        if (isset($args['headers']['Bcc'])) {
+            $recipients = array_merge($recipients, $rcube_imap->decode_address_list($args['headers']['Bcc']));
+        }
+
+        // stores contacts that don't exist in current address books
+        $new_contacts = array();
+
+        // iterate over recipients and search for them in address books
+        foreach ($recipients as $recipient) {
+            // flag to denote if the current recipient doesn't exist in any of the address books
+            $is_new_contact = true;
+
+            // interate over over address books and search for a contact with the same email address
+            foreach ($this->addressbooks as $abook_id => $address_source) {
+                $address_book = $this->rcmail->get_address_book($abook_id);
+                $search_result = $address_book->search('email', $recipient['mailto']);
+
+                // the contact already exist. skip the rest address books and move to next recipient
+                if ($search_result->count > 0) {
+                    $is_new_contact = false;
+                    break;
+                }
+            }
+
+            // store the non-existing recipient
+            if ($is_new_contact) {
+                $new_contacts[] = $recipient;
+            }
+        }
+
+        if (!empty($new_contacts)) {
+            $_SESSION['recipient_to_contact'] = $new_contacts;
+        }
+    }
+
+    /**
+     * Binds a JavaScript which renders a dialog for adding new contacts.
+     *
+     * @param array $args Default hook parameters.
+     *
+     * @return void
+     */
+    public function register_dialog($args)
+    {
+        if (isset($_SESSION['recipient_to_contact'])) {
+            $this->include_script('recipient_to_contact.js');
+        }
+
+        return $args;
+    }
+
+    /**
+     * Fetches new contacts and a list of available address books and calls a clients side script
+     * to process them.
+     *
+     * @return void
+     */
+    public function get_contacts()
+    {
+        if (!isset($_SESSION['recipient_to_contact'])) {
+            $this->rcmail->output->command('plugin.recipient_to_contact_populate_dialog', array());
+        } else {
+            $contacts = $_SESSION['recipient_to_contact'];
+            $address_books = $this->addressbooks;
+
+            $this->rcmail->session->remove('recipient_to_contact');
+
+            $this->rcmail->output->command('plugin.recipient_to_contact_populate_dialog', array(
+                'contacts'      => $contacts,
+                'address_books' => $address_books
+            ));
+        }
+    }
+
+    /**
+     * Adds contacts to selected addressbook.
+     *
+     * @param array $args  Handler's default arguments.
+     *
+     * @return void
+     */
+    public function add_contact($args)
+    {
+        // response that will be sent to client
+        $response = array();
+
+        // get request data
+        $contacts = get_input_value('_contacts', RCUBE_INPUT_POST);
+        $addressbook_id = get_input_value('_addressbook', RCUBE_INPUT_POST);
+
+        // iterate over each contact, validate and create new permament contacts
+        foreach ($contacts as $key => $contact) {
+            $response[$key] = array('status' => '', 'message' => '');
+
+            // name cannot be empty
+            if (empty($contact['_name'])) {
+                $response[$key]['status'] = 'fail';
+                $response[$key]['message'] = Q($this->gettext('response_name_empty'));
+                continue;
+            }
+
+            // email should be valid
+            if (!check_email($contact['_email'], false)) {
+                $response[$key]['status'] = 'fail';
+                $response[$key]['message'] = Q($this->gettext('response_email_invalid'));
+                continue;
+            }
+
+            $addressbook = $this->rcmail->get_address_book($addressbook_id);
+
+            // create new contact and check, if it was successful
+            if ($addressbook->insert(
+                    array('name' => $contact['_name'], 'email' => $contact['_email'])) == false) {
+                $response[$key]['status'] = 'fail';
+                $response[$key]['message'] = Q($this->gettext('response_server_error'));
+                continue;
+            }
+
+            $response[$key]['status'] = 'ok';
+            $response[$key]['message'] = Q($this->gettext('response_confirm'));
+        }
+
+        // return reponse to client
+        $response = array('contacts' => $response);
+        $this->rcmail->output->command('plugin.recipient_to_contact_add_contact_response', $response);
+        
+    }
+
+    /**
+     * Shows an option in prefrences section to enable or disable the plugin.
+     *
+     * @param array $args Default hook arguments.
+     *
+     * @return array
+     */
+    public function prefs_content($args)
+    {
+        // ensure we are in the right section
+        if ($args['section'] == 'recipienttocontact') {
+
+            $field_id = 'rcmfd_use_recipienttocontact';
+
+            // current status: plugin can be enabled or disabled
+            $use_recipienttocontact = $this->rcmail->config->get('use_recipienttocontact');
+
+            // checkbox element to change the status of the plugin
+            $checkbox = new html_checkbox(
+                    array('name' => '_use_recipienttocontact', 
+                          'id' => $field_id,
+                          'value' => 1,
+                          'style' => 'margin-left: -300px')
+            );
+            
+            $args['blocks']['recipienttocontact']['options']['description'] = array(
+                'title' =>   html::div(null, Q($this->gettext('prefs_descr'))) . html::br(),
+                'content' => ''
+            );
+
+            $args['blocks']['recipienttocontact']['name'] = $this->gettext('prefs_title');
+            $args['blocks']['recipienttocontact']['options']['use_subscriptions'] = array(
+                'title' => html::label($field_id, Q($this->gettext('prefs_option'))),
+                'content' => $checkbox->show($use_recipienttocontact ? 1 : 0),
+            );
+        }
+
+        return $args;
+    }
+
+    /**
+     * Adds a link to prefrences section.
+     *
+     * @param array $args Default hook arguments.
+     *
+     * @return array
+     */
+    public function prefs_section_link($args)
+    {
+		$args['list']['recipienttocontact'] = array(
+            'id' => 'recipienttocontact',
+			'section' => Q($this->gettext('prefs_title'))
+        );
+
+		return $args;
+    }
+
+    /**
+     * Saves the preferences.
+     *
+     * @param array $args Default hook arguments.
+     *
+     * @return array
+     */
+    public function prefs_save($args)
+    {
+        // ensure that we are in the relevant section
+        if ($args['section'] == 'recipienttocontact') {
+            // check if the plugin has been activated or deactivated
+            if (get_input_value('_use_recipienttocontact', RCUBE_INPUT_POST) !== null) {
+                $args['prefs']['use_recipienttocontact'] = true;
+            } else {
+                $args['prefs']['use_recipienttocontact'] = false;
+            }
+        }
+        
+        return $args;
+    }
+
+    /**
+     * Returns a list of addressbooks, filtered by ids.
+     *
+     * Output format identical to rcmail::get_address_sources().
+     *
+     * @param array $ids      IDs of addressbooks.
+     * @param bool  $writable If set to true, fetches only writable addressbooks.
+     *
+     * @see rcmail::get_address_sources().
+     *
+     * @return array Addressbooks sources.
+     */
+    protected function get_addressbooks(array $ids, $writable = false)
+    {
+        $ids = array_flip($ids);
+
+        // handle 'sql' addressbook properly: get_address_sources identifies sql addressbook as '0'
+        if (isset($ids['sql'])) {
+            unset($ids['sql']);
+            $ids[0] = 0;
+        }
+        $all_addresbooks = $this->rcmail->get_address_sources();
+
+        // return standard output from get_address_sources including only addressbooks specified in $ids
+        return array_intersect_key($all_addresbooks, $ids);
+    }
+}
